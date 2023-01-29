b07bbaba962300e817b44bdda4b2035f481f228e
Julie Koubová <julie@koubova.net>
2023-01-29 01:16:50 +0100

    g/gg end/home

--- keyboards/keychron/q4/ansi_v2/keymaps/juliekoubova/keymap.c
+++ keyboards/keychron/q4/ansi_v2/keymaps/juliekoubova/keymap.c
@@ -16,7 +16,6 @@
 
 #include QMK_KEYBOARD_H
 
-// clang-format off
 
 enum layers {
     BASE,
@@ -24,58 +23,68 @@ enum layers {
 };
 
 enum key_codes {
-    KC_FN_B = SAFE_RANGE,
-    KC_FN_F,
+    QK_VIM_B = SAFE_RANGE,
+    QK_VIM_F,
 };
+
+enum tap_dances {
+    TD_VIM_G,
+};
+
+// clang-format off
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [BASE] = LAYOUT_ansi_61(
-        KC_ESC,  KC_1,     KC_2,        KC_3,    KC_4,    KC_5,    KC_6,     KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,  KC_Q,     KC_W,        KC_E,    KC_R,    KC_T,    KC_Y,     KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-        TT(FN),  KC_A,     KC_S,        KC_D,    KC_F,    KC_G,    KC_H,     KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
-        KC_LSFT,           KC_Z,        KC_X,    KC_C,    KC_V,    KC_B,     KC_N,    KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL, KC_LOPT,  KC_LCMD,                                KC_SPC,                               TT(FN),   KC_RCMD,  KC_ROPT,  KC_RCTL),
+        KC_ESC,      KC_1,     KC_2,        KC_3,    KC_4,     KC_5,         KC_6,       KC_7,    KC_8,    KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
+        KC_TAB,      KC_Q,     KC_W,        KC_E,    KC_R,     KC_T,         KC_Y,       KC_U,    KC_I,    KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        TT(FN),      KC_A,     KC_S,        KC_D,    KC_F,     KC_G,         KC_H,       KC_J,    KC_K,    KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_LSFT,     KC_Z,     KC_X,        KC_C,    KC_V,     KC_B,         KC_N,       KC_M,    KC_COMM, KC_DOT,   KC_SLSH,                      KC_RSFT,
+        KC_LCTL,     KC_LOPT,  KC_LCMD,                                      KC_SPC,                                 TT(FN),   KC_RCMD,  KC_ROPT,  KC_RCTL),
 
     [FN] = LAYOUT_ansi_61(
-        KC_TILD, KC_F1,    KC_F2,       KC_F3,   KC_F4,   KC_F5,   KC_F6,      KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_LEFT,
-        XXXXXXX, XXXXXXX,  C(KC_RIGHT), XXXXXXX, KC_INS,  XXXXXXX, XXXXXXX,    XXXXXXX, KC_HOME, XXXXXXX,  XXXXXXX,  KC_VOLD,  KC_VOLU,  KC_MUTE,
-        _______, KC_END,   XXXXXXX,     XXXXXXX, KC_FN_F, XXXXXXX, KC_LEFT,    KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX,  XXXXXXX,            XXXXXXX,
-        _______,           XXXXXXX,     KC_DEL,  XXXXXXX, XXXXXXX, KC_FN_B,    XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,            _______,
-        _______, _______,  _______,                                KC_RIGHT,                               _______,  _______,  _______,  _______)
+        KC_TILD,     KC_F1,    KC_F2,       KC_F3,   KC_F4,    KC_F5,        KC_F6,      KC_F7,   KC_F8,   KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_LEFT,
+        XXXXXXX,     XXXXXXX,  C(KC_RIGHT), XXXXXXX, KC_INS,   XXXXXXX,      XXXXXXX,    XXXXXXX, KC_HOME, XXXXXXX,  XXXXXXX,  KC_VOLD,  KC_VOLU,  KC_MUTE,
+        _______,     KC_END,   XXXXXXX,     XXXXXXX, QK_VIM_F, TD(TD_VIM_G), KC_LEFT,    KC_DOWN, KC_UP,   KC_RIGHT, XXXXXXX,  XXXXXXX,            XXXXXXX,
+        _______,               XXXXXXX,     KC_DEL,  XXXXXXX,  XXXXXXX,      QK_VIM_B,   XXXXXXX, XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,            _______,
+        _______,     _______,  _______,                                      KC_RIGHT,                               _______,  _______,  _______,  _______)
 };
 
 // clang-format on
 
-#define FN_BITMASK (1 << FN)
+// ============================================================================
+// Key Overrides
+// ============================================================================
+
+#define LAYER_BIT(layer) (1 << layer)
 
 // Fn+X       = forward delete
 // Fn+Shift+X = backspace
 const key_override_t fn_shift_x = ko_make_with_layers(
     MOD_MASK_SHIFT, KC_DEL,
     KC_BSPC,
-    FN_BITMASK
+    LAYER_BIT(FN)
 );
 
 // Fn+B = Ctrl-left (jump one word to the left)
 const key_override_t fn_b = ko_make_with_layers_and_negmods(
-    0, KC_FN_B,
+    0, QK_VIM_B,
     C(KC_LEFT),
-    FN_BITMASK,
+    LAYER_BIT(FN),
     MOD_MASK_CTRL
 );
 
 // Fn+Ctrl+B = Page Up
 const key_override_t fn_ctrl_b = ko_make_with_layers(
-    MOD_MASK_CTRL, KC_FN_B,
+    MOD_MASK_CTRL, QK_VIM_B,
     KC_PAGE_UP,
-    FN_BITMASK
+    LAYER_BIT(FN)
 );
 
 // Fn+Ctrl+F = Page Down
 const key_override_t fn_ctrl_f = ko_make_with_layers(
-    MOD_MASK_CTRL, KC_FN_F,
+    MOD_MASK_CTRL, QK_VIM_F,
     KC_PAGE_DOWN,
-    FN_BITMASK
+    LAYER_BIT(FN)
 );
 
 
@@ -86,6 +95,20 @@ const key_override_t **key_overrides = (const key_override_t*[]){
     &fn_ctrl_f,
     NULL
 };
+
+// ============================================================================
+// Tap Dances
+// ============================================================================
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [TD_VIM_G] = ACTION_TAP_DANCE_DOUBLE(C(KC_END), C(KC_HOME)),
+};
+
+// ============================================================================
+// RGB Matrix
+// ============================================================================
+
+#ifdef RGB_MATRIX_ENABLE
 
 bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     uint8_t layer = get_highest_layer(layer_state);
@@ -103,10 +126,10 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                     rgb_matrix_set_color(index, RGB_RED);
                     continue;
                 }
-            }
+            };
 
             uint16_t keycode = keymap_key_to_keycode(BASE, (keypos_t){col,row});
-            if (keycode == KC_LSFT || keycode == KC_RSFT) {
+            if (keycode == KC_LSFT) {
                 if (is_caps_word_on() || host_keyboard_led_state().caps_lock) {
                     rgb_matrix_set_color(index, RGB_TURQUOISE);
                     continue;
@@ -119,6 +142,4 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     return false;
 }
 
-// void caps_word_set_user(bool active) {
-//     rgb_matrix_indicators();
-// }
+#endif
